e9ccc6666fe6e8f7e3d9969dc8c9b7adae065fe0
MyeongJun Kim <johnkim682@gmail.com>
2024-04-15 20:47:38 +0900

    Erased HTTP parser

--- settings.py
+++ settings.py
@@ -3,10 +3,6 @@ import json
 API_URL = 'https://api.github.com'
 HTML_URL = 'https://github.com'
 # --------------------------------- #
-HTTP_COMMIT = '#repo-content-pjax-container > div > div > div.Layout.Layout--flowRow-until-md.react-repos-overview-margin.Layout--sidebarPosition-end.Layout--sidebarPosition-flowRow-end > div.Layout-main > react-partial > div > div > div.Box-sc-g0xbh4-0.yfPnm > div:nth-child(1) > table > tbody > tr.Box-sc-g0xbh4-0.jEbBOT > td > div > div.Box-sc-g0xbh4-0.jGfYmh > a > span > span:nth-child(2) > span'
-HTTP_OPEN_ISSUE = 'a[data-ga-click="Issues, Table state, Open"]'
-HTTP_CLOSED_ISSUE = 'a[data-ga-click="Issues, Table state, Closed"]'
-
 
 # --------------------------------- #
 
