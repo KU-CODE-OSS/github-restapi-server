9f25c4e2f3719e67e13f47c26ea8bb0764ded37e
johnkim6823 <johnkim682@gmail.com>
2024-11-06 20:01:17 +0900

    interver updated

--- utils/settings.py
+++ utils/settings.py
@@ -6,7 +6,7 @@ HTML_URL = 'https//github.com'
 GITHUB_TOKEN_FILE = 'GITHUB_TOKEN'
 remaining_requests = 0
 current_token = None
-REQ_DELAY = 0.1
+REQ_DELAY = 0.5
 
 async def get_github_tokens():
     with open(GITHUB_TOKEN_FILE, 'r') as file:
